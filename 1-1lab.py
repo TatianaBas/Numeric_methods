ca74dea5c560787fc2b02485479f4e0e57789a39
Tatiana <tany14bas@yandex.ru>
2025-02-20 12:09:19 +0300

    Наконец-то нормальные изменения в 1 лабе

--- 1-1lab.py
+++ 1-1lab.py
@@ -36,7 +36,7 @@ def read_tridiagonal_matrix(n):
 def solve(A, b):
     n = len(A)
     ok = True
-    for i in range(1, n-2):
+    for i in range(1, n-1):
         if abs(A[i][i]) < abs(A[i][i-1]) + abs(A[i][i+1]):
             ok = False
             return ValueError("Достаточное условие не выполнено!")
