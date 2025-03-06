321debeb43247644614780863422d54fa0d2c67f
Tatiana <tany14bas@yandex.ru>
2025-03-06 11:59:17 +0300

    normal round in 1-3lab

--- 1-3lab.py
+++ 1-3lab.py
@@ -15,22 +15,33 @@
 Вывод:
 
 Iteration method
-[1.0000054874029314, 2.000002710606859, 2.999996803157834, -1.9999958525730877]
+[1.0, 2.0, 3.0, -2.0]
 Iterations: 10
 
 Seidel method
-[0.9999824807788378, 1.9999990888927066, 2.99999678635388, -2.0000005516184944]
+[1.0, 2.0, 3.0, -2.0]
 Iterations: 6
 
-
 """
+
 import math
 
 def L2_norm(X):
     """
     Вычисляет норму вектора L2.
     """
-    return math.sqrt(sum(x * x for x in X))
+    l2_norm = sum(x * x for x in X)
+    return math.sqrt(l2_norm)
+
+def matrix_vector_multiplication(A, x):
+    """
+    Умножает матрицу A на вектор x.
+    """
+    result = [0] * len(A)
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            result[i] += A[i][j] * x[j]
+    return result
 
 def is_diagonally_dominant(A):
     """
@@ -52,12 +63,12 @@ def is_diagonally_dominant2(A):
             return False
     return True
 
+
 def solve_iterative(A, b, eps):
     """
     Метод простых итераций для решения Ax = b.
     """
     n = len(A)
-    decimal_places = len(str(eps).split('.')[-1]) if '.' in str(eps) else 0
     iterations = 0
     cur_x = [b[i] / A[i][i] for i in range(n)]  # Начальное приближение
     converge = False
@@ -65,7 +76,7 @@ def solve_iterative(A, b, eps):
     while not converge:
         prev_x = cur_x[:]
         cur_x = [
-            round(sum(-A[i][j] / A[i][i] * prev_x[j] for j in range(n) if i != j) + b[i] / A[i][i], decimal_places)
+            sum(-A[i][j] / A[i][i] * prev_x[j] for j in range(n) if i != j) + b[i] / A[i][i]
             for i in range(n)
         ]
         iterations += 1
@@ -73,12 +84,12 @@ def solve_iterative(A, b, eps):
 
     return cur_x, iterations
 
+
 def solve_seidel(A, b, eps):
     """
     Метод Зейделя для решения Ax = b без хранения alpha и beta.
     """
     n = len(A)
-    decimal_places = len(str(eps).split('.')[-1]) if '.' in str(eps) else 0
     iterations = 0
     cur_x = [b[i] / A[i][i] for i in range(n)]  # Начальное приближение
     converge = False
@@ -88,31 +99,36 @@ def solve_seidel(A, b, eps):
         for i in range(n):
             sum_val = sum(-A[i][j] / A[i][i] * cur_x[j] for j in range(i)) + \
                       sum(-A[i][j] / A[i][i] * prev_x[j] for j in range(i + 1, n))
-            cur_x[i] = round(sum_val + b[i] / A[i][i], decimal_places)
+            cur_x[i] = sum_val + b[i] / A[i][i]
         iterations += 1
         converge = L2_norm([prev_x[i] - cur_x[i] for i in range(n)]) <= eps
 
     return cur_x, iterations
 
+
 if __name__ == '__main__':
     n = int(input('Enter the size of matrix: '))
+
     print('Enter matrix A')
     A = [list(map(float, input().split())) for _ in range(n)]
+
     print('Enter vector b')
     b = list(map(float, input().split()))
+
     eps = float(input('Enter epsilon: '))
-    
+
     if not is_diagonally_dominant(A) and not is_diagonally_dominant2(A):
         raise ValueError("Матрица не удовлетворяет достаточному условию единственности решения (диагональное преобладание).")
-    
+    decimal_places = len(str(eps).split('.')[-1]) if '.' in str(eps) else 0
     print('Iteration method')
     x_iter, i_iter = solve_iterative(A, b, eps)
+    x_iter = [round(x, decimal_places) for x in x_iter]
     print(x_iter)
     print('Iterations:', i_iter)
     print()
-    
+
     print('Seidel method')
     x_seidel, i_seidel = solve_seidel(A, b, eps)
+    x_seidel = [round(x, decimal_places) for x in x_seidel]
     print(x_seidel)
     print('Iterations:', i_seidel)
-
