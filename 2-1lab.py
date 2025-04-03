4d0970355f6e4e065905941928a05af7646d4900
Tatiana <tany14bas@yandex.ru>
2025-04-03 12:17:31 +0300

    Finish lab2-1

--- 2-1lab.py
+++ 2-1lab.py
@@ -16,6 +16,8 @@ x = 0.0915, f(x) = -0.0
 x = 0.0915, f(x) = 0.0
 """
 
+# Решение методом Ньютона и простых итераций решение уравнения sqrt(1 - x**2) - exp(x) + 0.1 = 0
+
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -42,32 +44,78 @@ def phi(x):
 def phi_derivative(x):
     return -x / (math.sqrt(1 - x**2) * (math.sqrt(1 - x**2) + 0.1))
 
-def iteration_method(x, eps):
-    x_prev = x
-    i = 0
-    while True:
-        i += 1
-        x = phi(x_prev)
-        if abs(f(x) - f(x_prev)) < eps:
+def iteration_method():
+    while(1):
+        print('Введите концы отрезка')
+        a, b = map(float, input().split())
+        precision = float(input('Введите точность: '))
+        decimal_places = get_decimal_places(precision)
+        if(not(phi_derivative(a) < 1 and phi_derivative(b) < 1)):
+            print("Производная функции на концах отрезка не меньше 1! Метод простых итераций неприменим.")
+        else:
+            x=(a-b)/2 
             break
+    while(1):
         x_prev = x
+        i = 0
+        while (1):
+            i += 1
+            x = phi(x_prev)
+            if abs(f(x) - f(x_prev)) < precision:
+                break
+            x_prev = x
+        break
     print(f"Число итераций: {i}")
-    return x
+    return x, decimal_places
 
-def NewtonsMethod(x, eps):
-    if abs(x) >= 1:
-        raise ValueError("x должен быть в пределах (-1,1), так как sqrt(1 - x^2) определена только в этом интервале.")
-    if(not(f(x) * second_derivative(x) < 2 * (derivative(x))**2)):
-        raise ValueError("Не выполняется неравенство второй производной")
-    prev = x
-    x = x - f(x)/derivative(x)
-    i = 1
-    while abs(x - prev) > eps:
-        prev = x
-        x = x - f(x)/derivative(x)
-        i += 1
-    print(f"Число итераций: {i}")
-    return x
+def initialX(x):  
+    return second_derivative(x) * f(x) > 0
+
+
+def convergence_condition(x):  # достаточное условие сходимости
+    return abs(f(x) * second_derivative(x)) < abs(derivative(x)) ** 2
+
+def check_conditions_newton(a, b):
+    fact = True
+    if convergence_condition(a) and convergence_condition(b):
+        if initialX(a):
+            x = a
+        else:
+            x = b
+        print(f"Условие выполнено в обеих точках, выбираем ближайшую")
+    elif convergence_condition(a):
+        print(f"Условие не выполнено в точке {b}, берем {a}")
+        x = a
+    elif convergence_condition(b):
+        print(f"Условие не выполнено в точке {a}, берем {b}")
+        x = b
+    else:
+        print(f"Не выполняется условие сходимости в точках {a} и {b}")
+        fact = False
+    return fact, x
+
+def NewtonsMethod():
+    """Найти корень методом Ньютона."""
+    counter = 0
+    ans = 0
+    while(1):
+        print('Введите концы отрезка')
+        a, b = map(float, input().split())
+        precision = float(input('Введите точность: '))
+        decimal_places = get_decimal_places(precision)
+        fact, ans = check_conditions_newton(a,b)
+        if(fact):
+            break
+    while (1):
+        counter += 1
+        new_ans = ans - f(ans)/derivative(ans)
+        if (abs(new_ans-ans)<precision):
+            ans = new_ans
+            break
+        ans = new_ans
+    print(f"Число итераций: {counter}")
+    return ans, decimal_places
+
 
 # Строим график
 def show():
@@ -88,27 +136,18 @@ def show():
     return
 
 
-"""
-корень: [a, b] = [0; 0.5]
-"""
 
 
 if __name__ == "__main__":
     show()
-    print('Введите концы отрезка')
-    l, r = map(float, input().split())
-    eps = float(input('Введите точность: '))
-    decimal_places = get_decimal_places(eps)
 
     print('Метод Ньютона')
-    x = NewtonsMethod((l-r)*0.5, eps)
+    x, dec = NewtonsMethod()
     func = f(x)
-    print(f"x = {round(x, decimal_places)}, f(x) = {round(func, decimal_places)}\n")
+    print(f"x = {round(x, dec)}, f(x) = {round(func, dec)}")
 
-    if(not(phi_derivative(l) < 1 and phi_derivative(r) < 1)):
-        raise ValueError("Производная функции на концах отрезка не меньше 1! Метод простых итераций неприменим.")
     print('Метод простых итераций')
-    x = iteration_method((l-r)*0.5, eps)
+    x, dec = iteration_method()
     func = f(x)
-    print(f"x = {round(x, decimal_places)}, f(x) = {round(func, decimal_places)}")
+    print(f"x = {round(x, dec)}, f(x) = {round(func, dec)}")
 
